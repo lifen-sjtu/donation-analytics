825198309147efdee3fb2570369c61a2459b780a
Fen Li <lifensjtu@gmail.com>
2018-02-11 22:08:38 -0800

    read percentile from file

--- src/com/insights/data/Main.java
+++ src/com/insights/data/Main.java
@@ -12,11 +12,16 @@ public class Main {
 
     public static void main(String[] args) {
         String itcont = args[0];
+        String percentileFile = args[1];
         String line;
         try {
+            FileReader percentileReader = new FileReader(percentileFile);
+            BufferedReader percentileBuffer = new BufferedReader(percentileReader);
+            int percentile = Integer.valueOf(percentileBuffer.readLine());
+            percentileReader.close();
             FileReader fileReader = new FileReader(itcont);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            ContributionProcessor contributionProcessor = new ContributionProcessor(30);
+            ContributionProcessor contributionProcessor = new ContributionProcessor(percentile);
             while ((line = bufferedReader.readLine()) != null) {
                 try {
                     Contribution contribution = new Contribution(line);
@@ -25,6 +30,7 @@ public class Main {
                     // simply skip invalid input contribution record
                 }
             }
+            fileReader.close();
         } catch (IOException ex) {
             System.out.println("unable to read or write file ");
         }
